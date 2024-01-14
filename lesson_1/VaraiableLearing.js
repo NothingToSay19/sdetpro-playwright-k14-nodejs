1cd5ad34397f828cb8e616e24f843a5f942846af
Huy Le <hule@axon.com>
2024-01-14 21:02:33 +0700

    init commit

---
+++ lesson_1/VaraiableLearing.js
@@ -0,0 +1,6 @@
+let myName = 'Huy'
+myName = 18
+//1.7976931348623157e+308 = 1.7976931348623157000000000....000 (22 chu so 0)
+// e+308 la 10 ^ 38
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
